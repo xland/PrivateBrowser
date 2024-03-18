017ea35e6b6052f1123aaea2f955995708a90267
unknown <412588801@qq.com>
2024-03-18 13:27:56 +0800

    layout

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # PrivateBrowser
 A browser that can be customized freely
+
+
+# Deploy
+
+D:\tool\FUCK\6.7.0\msvc2019_64\bin\windeployqt.exe privatebrowser.exe
--- src/PrivateBrowser.cpp
+++ src/PrivateBrowser.cpp
@@ -1,4 +1,5 @@
 #include <QHBoxLayout>
+#include <QVBoxLayout>
 #include <QSplitter>
 #include <QTextEdit>
 
@@ -9,8 +10,15 @@ PrivateBrowser::PrivateBrowser(QWidget *parent)
 {
     auto splitter = new QSplitter(Qt::Horizontal, this);
     auto textLeft = new QTextEdit("左侧", splitter);
-    auto textRight = new QTextEdit("右侧", splitter);
-    splitter->setStretchFactor(1, 1);
+    auto widget = new QWidget(splitter);
+    auto vboxLayout = new QVBoxLayout(splitter);
+
+    auto textRight1 = new QTextEdit("右侧1");
+    auto textRight2 = new QTextEdit("右侧2");
+    vboxLayout->addWidget(textRight1);
+    vboxLayout->addWidget(textRight2);
+
+    widget->setLayout(vboxLayout);
     setCentralWidget(splitter);
 }
 
