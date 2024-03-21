e482d7a4a89fdbb3aa23191115e4e72801fea350
unknown <412588801@qq.com>
2024-03-21 20:13:53 +0800

    a

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <Windows.h>
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
+{
+    auto result = CoInitializeEx(nullptr, COINIT_APARTMENTTHREADED);
+    if (result != S_OK)
+    {
+        return 0;
+    }
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    CoUninitialize();
+    return 0;
+}
